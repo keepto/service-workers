8b6bc62c0c7edeaf181d38d47b90d11aaa244591
huangmei <hm5yes@163.com>
2019-01-05 15:27:19 +0800

    url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in window.navigator) {
-  navigator.serviceWorker.register('./sw.js', { scope: './' })
+  navigator.serviceWorker.register('/service-workers/sw.js', { scope: '/service-workers/' })
     .then(function (reg) {
       console.log('register success')
       this.addEventListener('install', function (event) {
@@ -7,8 +7,8 @@ if ('serviceWorker' in window.navigator) {
         event.waitUntil(
           caches.open('img_storage').then(function (cache) {
             return cache.addAll([
-              './img/1.jpg',
-              './img/2.jpg'
+              '/service-workers/img/1.jpg',
+              '/service-workers/img/2.jpg'
             ])
           }
           ));

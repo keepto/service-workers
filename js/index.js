99b7b4376e48e1a399f2e7175ca465fdcfa2ff08
huangmei <hm5yes@163.com>
2019-01-05 11:04:59 +0800

    url

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in window.navigator) {
-  navigator.serviceWorker.register('./sw.js', { scope: './js' })
+  navigator.serviceWorker.register('./js/sw.js', { scope: './' })
     .then(function (reg) {
       const messageChannel = new MessageChannel();
       console.log(messageChannel)

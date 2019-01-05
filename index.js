9114943f6a3c845aeed4ff65a03a65b65e934853
huangmei <hm5yes@163.com>
2019-01-05 15:32:19 +0800

    url

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ if ('serviceWorker' in window.navigator) {
   navigator.serviceWorker.register('/service-workers/sw.js', { scope: '/service-workers/' })
     .then(function (reg) {
       console.log('register success')
-      this.addEventListener('install', function (event) {
-        console.log('install');
-        event.waitUntil(
-          caches.open('img_storage').then(function (cache) {
-            return cache.addAll([
-              '/service-workers/img/1.jpg',
-              '/service-workers/img/2.jpg'
-            ])
-          }
-          ));
-      });
     
     })
     .catch((err) => {
--- sw.js
+++ sw.js
@@ -1,6 +1,13 @@
 // 注册
 this.addEventListener('install', function (event) {
   console.log('Service Worker install');
+  event.waitUntil(
+    caches.open('img_storage').then(function (cache) {
+      return cache.addAll([
+        '/service-workers/img/1.jpg',
+        '/service-workers/img/2.jpg'
+      ])
+    }));
 })
 
 // 激活

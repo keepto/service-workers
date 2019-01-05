6ad0f2121df795c4d60da059127fef9d0941308a
huangmei <hm5yes@163.com>
2019-01-05 14:26:42 +0800

    img

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ if ('serviceWorker' in window.navigator) {
   navigator.serviceWorker.register('./sw.js', { scope: './' })
     .then(function (reg) {
       console.log('register success')
-      this.addEventListener('fetch', function (event) {
-        console.log(event.request.url);
-        event.respondWith(
-          caches.match(event.request).then(res => {
-            return res ||
-              fetch(event.request)
-                .then(responese => {
-                  const responeseClone = responese.clone();
-                  caches.open('sw_demo').then(cache => {
-                    cache.put(event.request, responeseClone);
-                  })
-                  return responese;
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-          })
-        )
+      this.addEventListener('install', function (event) {
+        console.log('install');
+        event.waitUntil(
+          caches.open('img_storage').then(function (cache) {
+            return cache.addAll([
+              './img/1.jpg',
+              './img/2.jpg'
+            ])
+          }
+          ));
       });
+    
     })
     .catch((err) => {
       console.log('register fail ',err)

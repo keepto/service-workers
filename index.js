536d23827b186aae54ebfc202ffc45ef4dc53bb7
huangmei <hm5yes@163.com>
2019-01-07 11:04:45 +0800

    modify file url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in window.navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '/' })
+  navigator.serviceWorker.register('./sw.js', { scope: './' })
     .then(function (reg) {
       console.log('register success')
     
